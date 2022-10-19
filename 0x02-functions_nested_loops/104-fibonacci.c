6876ef5ed97e07ba4dccb69da5ca32cfa8070629
SunmyVidal <faloyemosunmoluwadavid@gmail.com>
2022-10-18 19:52:29 -0700

    wow 3

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,7 +5,6 @@
 * @num : operand number
 * Return: number of digits
 */
-
 int numLength(int num)
 {
 	int length = 0;
@@ -26,7 +25,6 @@ int numLength(int num)
 *main - prints the first 98 fibonaci sequences
 *  Return: 0
 */
-
 	int main(void)
 	{
 		unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f1o = 0, f2o = 0, tmpo = 0;
